7177d5f99fef0b61b545ab419d2e9ce15089b5de
Emir Zakrewski <frenkchambers@gmail.com>
2023-12-07 23:01:56 +0300

    🚧: work on index page

--- offers/views.py
+++ offers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+
+from locations.models import City
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'offers/index.html')
