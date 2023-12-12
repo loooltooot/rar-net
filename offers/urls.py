87f0c0998cb27bb4304f2f0d8a4b5eeb362eeb1c
Emir Zakrewski <frenkchambers@gmail.com>
2023-12-12 17:41:16 +0300

    ✨: offers creation

--- offers/urls.py
+++ offers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, CreateOffer
 
 app_name = 'offers'
 urlpatterns = [
     path('', index, name='index'),
+    path('offers/new/', CreateOffer.as_view(extra_context={'title': 'Новая заявка'}), name='add_offer'),
 ]
--- offers/views.py
+++ offers/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.views import generic
-
 from locations.models import City
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import OfferForm
+from .models import Photo
 
 # Create your views here.
 
 def index(request):
     return render(request, 'offers/index.html')
+
+class CreateOffer(generic.CreateView, LoginRequiredMixin):
+    template_name = 'offers/add_offer.html'
+    redirect_field_name = ''
+    form_class = OfferForm
+
+    def get_success_url(self):
+        return reverse_lazy('offers:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        self.object = form.save()
+
+        images = form.cleaned_data['imgs']
+        for img in images:
+            Photo.objects.create(img=img, offer=self.object)
+
+        return HttpResponseRedirect(self.get_success_url())
